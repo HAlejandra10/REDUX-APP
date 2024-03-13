a0b50a291d611803dfdd1a9d0ee06b4150d6f375
Heidy Primo <heidyprimo10@hotmail.com>
2024-03-13 18:57:25 -0500

    adding useEffect and useDispatch hooks to handle states and to shot actions w/ dispatch to components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import './App.css';
 import Header from './components/Header';
+import Email from './components/Email';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { addUser} from "./redux/userSlice"
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.json())
+    .then((data)=> dispatch(addUser(data)))
+    .catch((error) => console.log(error));
+  }, []);
+
   return (
     <div className="App">
       <Header />
